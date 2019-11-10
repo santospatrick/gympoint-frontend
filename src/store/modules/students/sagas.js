6f8c5f14b8fea8fecf22b59af749a68a3e256c92
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-10 00:14:35 -0300

    adiciona tratamento de erro

--- src/store/modules/students/sagas.js
+++ src/store/modules/students/sagas.js
@@ -1,17 +1,23 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 
 import api from 'services/api';
+import { toast } from 'react-toastify';
 import { GET_STUDENTS_REQUEST } from './actionTypes';
-import { getStudentsSuccess } from './actions';
+import { getStudentsSuccess, getStudentsFailure } from './actions';
 
 function* setStudents({ payload }) {
-    const { search } = payload;
+    try {
+        const { search } = payload;
 
-    const response = yield call(api.get, 'students', {
-        params: { q: search },
-    });
+        const response = yield call(api.get, 'students', {
+            params: { q: search },
+        });
 
-    yield put(getStudentsSuccess(response.data));
+        yield put(getStudentsSuccess(response.data));
+    } catch (error) {
+        toast.error('Não foi possível listar alunos');
+        yield put(getStudentsFailure());
+    }
 }
 
 export default all([takeLatest(GET_STUDENTS_REQUEST, setStudents)]);
